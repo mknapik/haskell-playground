93f0990c371f41b117b3614d02c036f4ce246d28
Michał Knapik <mknapik@users.noreply.github.com>
2021-07-31 13:03:07 +0200

    Adjust names.txt format

--- src/Euler/Problem022.hs
+++ src/Euler/Problem022.hs
@@ -2,10 +2,14 @@ module Euler.Problem022 (euler22) where
 
 import Data.Char (digitToInt)
 import Data.List (sort)
-import Data.Numbers.Primes (primeFactors)
 import Data.Text.Internal.Unsafe.Char (ord)
 
-euler22 = (: []) . sum . zipWith (*) [1 ..] . map alphabetical . sort . words
+replace a b "" = ""
+replace a b (x : xs)
+  | a == x = b : replace a b xs
+  | otherwise = x : replace a b xs
+
+euler22 = (: []) . sum . zipWith (*) [1 ..] . map alphabetical . sort . words . replace ',' ' ' . replace '"' ' '
 
 begin = ord 'A' - 1
 
